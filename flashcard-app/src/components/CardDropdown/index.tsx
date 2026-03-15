cbceb72ed36a524ce1acd93d5da6909ff93bbbeb
Lena Le <lenanle333@gmail.com>
2026-03-15 16:11:31 -0700

    added delete modal

--- flashcard-app/src/components/CardDropdown/index.tsx
+++ flashcard-app/src/components/CardDropdown/index.tsx
@@ -3,21 +3,42 @@ import style from "./index.module.css";
 import editIcon from "../../assets/images/icon-edit.svg";
 import deleteIcon from "../../assets/images/icon-delete.svg";
 import EditCardModal from "../EditCardModal";
-export const CardDropdown = forwardRef<HTMLDivElement>(function CardDropdown(_, ref) {
-	const [editModalOpen, setEditModalOpen] = useState(false);
+import DeleteCardModal from "../DeleteCardModal";
+export const CardDropdown = forwardRef<HTMLDivElement>(
+	function CardDropdown(_, ref) {
+		const [editModalOpen, setEditModalOpen] = useState(false);
+		const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
-	return (
-		<div className={style.container} ref={ref}>
-			<div className={style.item} onClick={() => setEditModalOpen(!editModalOpen)}>
-				<img src={editIcon} alt="edit" />
-				<span>Edit</span>
+		return (
+			<div className={style.container} ref={ref}>
+				<div
+					className={style.item}
+					onClick={() => setEditModalOpen(!editModalOpen)}
+				>
+					<img src={editIcon} alt="edit" />
+					<span>Edit</span>
+				</div>
+				<div className="h-[0.0625rem] self-stretch bg-neutral-900" />
+				<div
+					className={style.item}
+					onClick={() => setDeleteModalOpen(!deleteModalOpen)}
+				>
+					<img src={deleteIcon} alt="delete" />
+					<span>Delete</span>
+				</div>
+				{editModalOpen ? (
+					<EditCardModal
+						isVisible={editModalOpen}
+						setIsVisible={setEditModalOpen}
+					/>
+				) : null}
+				{deleteModalOpen ? (
+					<DeleteCardModal
+						isVisible={deleteModalOpen}
+						setIsVisible={setDeleteModalOpen}
+					/>
+				) : null}
 			</div>
-			<div className="h-[0.0625rem] self-stretch bg-neutral-900" />
-			<div className={style.item}>
-				<img src={deleteIcon} alt="delete" />
-				<span>Delete</span>
-			</div>
-			{editModalOpen ? <EditCardModal /> : null}
-		</div>
-	);
-});
+		);
+	},
+);
